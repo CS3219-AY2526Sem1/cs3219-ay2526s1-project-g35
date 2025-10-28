6f30633d3900a440c908eed2404650336d3b752d
Johannsen <lumjohannsen@gmail.com>
2025-10-28 11:32:44 +0800

    style: Fix Prettier formatting for integration tests
    - Format jest.integration.config.js
    - Format src/test/integration/real-redis-jwt.test.js
    - All files now pass format:check ✅

--- backend/question-service/jest.integration.config.js
+++ backend/question-service/jest.integration.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    testEnvironment: 'node',
-    coveragePathIgnorePatterns: ['/node_modules/'],
-    testTimeout: 30000, // 30 seconds for integration tests
-    testMatch: ['**/src/test/integration/**/*.test.js'],
-    verbose: true,
-    collectCoverage: false,
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testTimeout: 30000, // 30 seconds for integration tests
+  testMatch: ['**/src/test/integration/**/*.test.js'],
+  verbose: true,
+  collectCoverage: false,
 };
-
